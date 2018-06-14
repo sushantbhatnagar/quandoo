c27b6fc79e8a2fa2bb5573928b6f98649647342e
Sushant Bhatnagar <sushantbhatnagar91@gmail.com>
2018-06-14 13:17:55 +0530

    Refactored Code and the naming of the local variable

--- features/step_definitions/step_def_sortable_data_table.rb
+++ features/step_definitions/step_def_sortable_data_table.rb
@@ -3,10 +3,10 @@ Given(/^I am on the Data Tables page$/) do
 end
 
 When(/^I sort the Last Name column in Example_II table in ([^"]*) order$/) do |sort_in|
-  @sorted_array_original = sort_in == 'ascending' ? on(DataTablePage).ascending_sort : on(DataTablePage).descending_sort
+  @before_click_sorted_names = sort_in == 'ascending' ? on(DataTablePage).ascending_sort : on(DataTablePage).descending_sort
 end
 
 Then(/^I should see the names sorted in (ascending|descending) order successfully$/) do |arg|
-  @after_click_sorted_array = on(DataTablePage).last_name_array
-  expect(@sorted_array_original).to eql @after_click_sorted_array
+  @after_click_sorted_names = on(DataTablePage).last_name_array
+  expect(@before_click_sorted_names).to eql @after_click_sorted_names
 end
