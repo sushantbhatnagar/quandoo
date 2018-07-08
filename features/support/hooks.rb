57e46398398cb33650e7f2a9a84c27a887cb3be3
Sushant Bhatnagar <sushantbhatnagar91@gmail.com>
2018-07-08 23:39:55 +0530

    Changes in hooks.rb file to use different browsers

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -9,12 +9,13 @@ Before do
   	end  	
 end
 
-# After do |scenario|
-#   if scenario.failed?
-#     time = Time.now.strftime("%Y-%m-%d_%H-%M-%S")
-#     filename = "error-#{@current_page.class}-#{time}.png"
-#     @current_page.save_screenshot(filename)
-#     embed(filename, 'image/png')
-#   end
-#   @browser.close
-# end
+After do |scenario|
+  require 'pry' ; binding.pry
+  if scenario.failed?
+    time = Time.now.strftime("%Y-%m-%d_%H-%M-%S")
+    filename = "error-#{@current_page.class}-#{time}.png"
+    @current_page.save_screenshot(filename)
+    embed(filename, 'image/png')
+  end
+  @browser.close
+end
