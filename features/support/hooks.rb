4662b64653846045cf2ffc62089c1ff753033af7
Sushant Bhatnagar <sushantbhatnagar91@gmail.com>
2018-07-22 01:37:56 +0530

    With and Without Zalenium Set up Configurations

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -1,20 +1,22 @@
 Before do
-  caps = Selenium::WebDriver::Remote::Capabilities.chrome("chromeOptions" => {"args" => [ "--disable-infobars" ]})
-  driver = Selenium::WebDriver.for :remote, url: 'http://localhost:4444/wd/hub', desired_capabilities: caps
-  @browser = Watir::Browser.new driver
-
-  case ENV['BROWSER']
-  	when 'firefox'
-  		@browser = Watir::Browser.new :firefox
-  	when 'chrome'
-  		@browser = Watir::Browser.new :chrome
-  	else
-  		@browser = Watir::Browser.new :ie
-  	end
+  if ENV['ZALENIUM']
+    caps = Selenium::WebDriver::Remote::Capabilities.chrome("chromeOptions" => {"args" => ["--disable-infobars"]})
+    driver = Selenium::WebDriver.for :remote, url: 'http://localhost:4444/wd/hub', desired_capabilities: caps
+    @browser = Watir::Browser.new driver
+  else
+    case ENV['BROWSER']
+      when 'firefox'
+        @browser = Watir::Browser.new :firefox
+      when 'chrome'
+        @browser = Watir::Browser.new :chrome
+      else
+        @browser = Watir::Browser.new :ie
+    end
+  end
 end
 
+
 After do |scenario|
-  require 'pry' ; binding.pry
   if scenario.failed?
     time = Time.now.strftime("%Y-%m-%d_%H-%M-%S")
     filename = "error-#{@current_page.class}-#{time}.png"
